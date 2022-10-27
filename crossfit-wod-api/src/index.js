63b02ac6402b97b56ee281d494db7d61a85ffbe6
Jake Isaacs <jisaacs5@vols.utk.edu>
2022-10-26 23:56:51 -0400

    Created base framework

---
+++ crossfit-wod-api/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express"); 
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+const v1Router = require("./v1/routes");
+
+app.use("/api/v1", v1Router);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
+
